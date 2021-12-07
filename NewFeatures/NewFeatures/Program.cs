9d3d6bc2a75035ee168029f777fdd9553e837d9f
Obi Oberoi <obi@eOberoi.com>
2021-12-07 18:23:01 -0500

    testing dateonly function

--- NewFeatures/NewFeatures/Program.cs
+++ NewFeatures/NewFeatures/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Obi Oberoi!!!");
+var today = DateOnly.FromDateTime(DateTime.Now);
+Console.WriteLine($"The date is: {today}");
 Console.ReadLine();
