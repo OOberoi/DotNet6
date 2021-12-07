e9bdc2c51fb8a80d7c246ae966a150ec6cdd0ae9
Obi Oberoi <obi@eOberoi.com>
2021-12-07 18:24:47 -0500

    testing timeonly function

--- NewFeatures/NewFeatures/Program.cs
+++ NewFeatures/NewFeatures/Program.cs
@@ -1,5 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Obi Oberoi!!!");
+
 var today = DateOnly.FromDateTime(DateTime.Now);
 Console.WriteLine($"The date is: {today}");
+
+var currTime = TimeOnly.FromDateTime(DateTime.Now);
+Console.WriteLine($"The time is: {currTime}");
+
 Console.ReadLine();
