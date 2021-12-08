c95f2b2e04f9bebb538c417fdd16d3953d18aae2
Obi Oberoi <obi@eOberoi.com>
2021-12-07 20:47:18 -0500

    testing environment vars

--- NewFeatures/NewFeatures/Program.cs
+++ NewFeatures/NewFeatures/Program.cs
@@ -7,4 +7,10 @@ Console.WriteLine($"The date is: {today}");
 var currTime = TimeOnly.FromDateTime(DateTime.Now);
 Console.WriteLine($"The time is: {currTime}");
 
+int processId = Environment.ProcessId;
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+string? myPath = Environment.ProcessPath.ToString();
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+
+Console.WriteLine($"Process Id: {processId} and Path: {myPath}");
 Console.ReadLine();
