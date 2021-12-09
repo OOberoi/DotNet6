a689cc67efbc4b52b8125d1d0dc5f666f168270a
Obi Oberoi <obi@eoberoi.com>
2021-12-08 23:59:12 -0500

    added a new param

--- NewFeatures/BlazorUI/Data/WeatherForecastService.cs
+++ NewFeatures/BlazorUI/Data/WeatherForecastService.cs
@@ -7,11 +7,12 @@ namespace BlazorUI.Data
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        public Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate)
+        public Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate, TimeOnly startTime )
         {
             return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date =  startDate.AddDays(index),
+                //Time = startTime.FromDateTime(DateTime.Now), //todod: to fix this
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             }).ToArray());
