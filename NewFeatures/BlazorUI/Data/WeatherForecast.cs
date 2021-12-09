70cfccdfa89a4fc47aab38980fb41f35d2cd8b50
Obi Oberoi <obi@eoberoi.com>
2021-12-08 21:43:18 -0500

    refactored the code in light of the new addition

--- NewFeatures/BlazorUI/Data/WeatherForecast.cs
+++ NewFeatures/BlazorUI/Data/WeatherForecast.cs
@@ -2,7 +2,7 @@ namespace BlazorUI.Data
 {
     public class WeatherForecast
     {
-        public DateTime Date { get; set; }
+        public DateOnly Date { get; set; }
         public TimeOnly Time { get; set; }
 
         public int TemperatureC { get; set; }
