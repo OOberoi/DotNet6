81c610cbb544ea3f47cfb67dcd4c3b1e454d5f85
Obi Oberoi <obi@eoberoi.com>
2021-12-10 20:39:48 -0500

    added props to the employee class

--- NewFeatures/Api/Employee.cs
+++ NewFeatures/Api/Employee.cs
@@ -2,5 +2,10 @@
 {
     public class Employee
     {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public char? Gender { get; set; }
+        public int Age { get; set; }
     }
 }
