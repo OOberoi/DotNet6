7cb94dda6e19af5693e62349332e05f82a9d107f
Obi Oberoi <obi@eOberoi.com>
2021-12-21 20:46:44 -0500

    removed errorneous code

--- NewFeatures/Api/Controllers/WeatherForecastController.cs
+++ NewFeatures/Api/Controllers/WeatherForecastController.cs
@@ -6,11 +6,6 @@ namespace Api.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Windy", "Balmy", "Hot", "Sweltering", "Scorching"    
-        };
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
